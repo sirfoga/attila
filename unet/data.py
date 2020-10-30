17a02213d0d20abfe0466a96e85d762c0c713260
stefano <sirfoga@protonmail.com>
2020-10-30 13:04:20 +0100

    weight init

--- unet/data.py
+++
@@ -1,9 +0,0 @@
-def crop_center(cropx,cropy):
-    def _f(img):
-        (y,x,_) = img.shape
-        startx = x//2-(cropx//2)
-        starty = y//2-(cropy//2)    
-        return img[starty:starty+cropy,startx:startx+cropx]
-    return _f
-
-# todo: random crop
