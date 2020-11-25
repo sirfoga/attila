d8e500c4cc12eeb6e211476e36eed69cf62eb225
stefano <sirfoga@protonmail.com>
2020-11-25 21:29:12 +0100

    reporting .tex of runs

--- attila/experiments/tools.py
+++ attila/experiments/tools.py
@@ -1,6 +1,9 @@
 import numpy as np
 from tensorflow.keras import backend as K
+
+from attila.experiments.data import load_experiments
 from attila.experiments.do import get_model
+from attila.util.config import is_verbose
 
 
 def create_tex_table_configurations(experiments, config):
@@ -43,7 +46,14 @@ def create_tex_table_results(experiments):
         for key in metric_keys
     }
 
+    out = {}
+
     for experiment in experiments:
+        out[experiment['name']] = {
+            key: experiment[key]
+            for key in metric_keys
+        }
+
         for key in metric_keys:
             if experiment[key] == best_values[key]:
                 experiment[key] = '\\textbf{{{:.3f}}}'.format(experiment[key])
@@ -57,9 +67,69 @@ def create_tex_table_results(experiments):
         )
         print(row_table)
 
+    return out
 
-def create_tex_experiments(experiments, config):
-    create_tex_table_configurations(experiments, config)
-    print()
 
-    create_tex_table_results(experiments)
+def create_tex_table_runs_results(runs):
+    row_table_f = '{} & {} & {} \\\\'
+    metric_keys = list(list(runs[0].values())[0].keys())  # keys of all models
+    model_names = list(runs[0].keys())
+
+    print('creating .tex table for {} runs\n'.format(len(runs)))
+
+    out = {
+        model: {
+            key: np.mean([
+                run[model][key]
+                for run in runs
+            ])  # across all runs
+            for key in metric_keys
+        }
+        for model in model_names
+    }
+
+    best_values = {
+        key: np.max([
+            out[model_name][key]
+            for model_name in model_names
+        ])  # across all models
+        for key in metric_keys
+    }
+
+    for model_name, results in out.items():
+        for key in metric_keys:
+            if results[key] == best_values[key]:
+                results[key] = '\\textbf{{{:.3f}}}'.format(results[key])
+            else:
+                delta = 100 - 100 * results[key] / best_values[key]
+                results[key] = '{:.3f} (-{:.1f} \%)'.format(results[key], delta)
+
+        row_table = row_table_f.format(
+            model_name,
+            *(results[key] for key in metric_keys)
+        )
+        print(row_table)
+
+    return out
+
+
+def create_tex_experiments(config, out_folder):
+    nruns = config.getint('experiments', 'nruns')
+    all_runs = []
+    for nrun in range(nruns):
+        folder = out_folder / 'run-{}'.format(nrun)
+        results_file = folder / config.get('experiments', 'output file')
+        results = load_experiments(results_file)
+
+        if is_verbose('experiments', config):
+            print('run #{}: loaded {} results from {}'.format(nrun + 1, len(results), results_file))
+
+        create_tex_table_configurations(results, config)
+        print()
+
+        run_results = create_tex_table_results(results)
+        print()
+
+        all_runs.append(run_results)
+
+    create_tex_table_runs_results(all_runs)
