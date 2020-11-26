60ad0048320794af624c8a985c5a921a90883650
stefano <sirfoga@protonmail.com>
2020-11-26 12:18:37 +0100

    try oy script

--- experiments.py
+++ experiments.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from attila.util.config import get_config
 from attila.data.parse import parse_data, get_data
-from attila.experiments.data import load_experiments, save_experiments
-from attila.experiments.do import do_experiments
+from attila.util.config import get_config
+from attila.experiments.data import load_experiments
+from attila.experiments.do import do_batch_experiments
+from attila.experiments.tools import create_tex_experiments
 
 _here = Path('.').resolve()
 
@@ -28,28 +29,31 @@ def check_input(config):
 
 
 def main():
-  config = get_config(_here / './config.ini')
-  check_input(config)
+    config = get_config(_here / './config.ini')
+    check_input(config)
 
-  data_path = _here / config.get('data', 'folder')
-  data_path = data_path.resolve()
+    config = get_config(_here / 'config.ini')
 
-  out_folder = Path(config.get('experiments', 'output folder')).resolve()
-  out_folder.mkdir(parents=True, exist_ok=True)  # rm and mkdir if existing
+    data_path = _here / config.get('data', 'folder')
+    data_path = data_path.resolve()
 
-  images_path = data_path / config.get('data', 'images')
-  masks_path = data_path / config.get('data', 'masks')
-  raw = get_data(images_path, masks_path)
-  X, y = parse_data(
-    raw,
-    (config.getint('image', 'width'), config.getint('image', 'height'))
-  )
+    out_path = Path(config.get('experiments', 'output folder')).resolve()
+    out_path.mkdir(parents=True, exist_ok=True)  # rm and mkdir if existing
 
-  # experiments_file = _here / config.get('experiments', 'output file')
-  experiments = load_experiments(experiments_file)
-  do_experiments(experiments, (X, y), config, out_folder)
+    images_path = data_path / config.get('data', 'images')
+    masks_path = data_path / config.get('data', 'masks')
 
-  # save_experiments(experiments, out_folder / config.get('experiments', 'output file'))
+    raw = get_data(images_path, masks_path)
+    X, y = parse_data(
+        raw,
+        (config.getint('image', 'width'), config.getint('image', 'height'))
+    )
+
+    experiments_file = _here / 'experiments.json'
+    experiments = load_experiments(experiments_file)
+
+    do_batch_experiments(experiments, (X, y), config, out_path)
+    create_tex_experiments(config, out_path, out_path / 'tables.tex')
 
 
 if __name__ == '__main__':
