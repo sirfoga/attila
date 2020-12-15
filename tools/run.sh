b2b1683cf87ab1bf8a96ec371b68b76ad2ad6810
stefano <sirfoga@protonmail.com>
2020-12-15 10:13:47 +0100

    add multiple reunners

--- tools/run.sh
+++ tools/run.sh
@@ -3,4 +3,7 @@ source activate ${KERNELS_DIR}/${KERNEL_NAME}  # or source
 echo "using $(which python)"  # just to check 
 
 rm -rf out/  # clean to be sure
-python train_and_eval.py
+
+# python train_and_eval.py
+# python to_augment_or_not.py
+python LOST.py
